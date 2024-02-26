0efafd9c91e606e94f6525f4dbcafe22dc1393de
zhalgasbolatov <gfk575@gmail.com>
2024-02-26 19:12:27 +0600

    7 COMMIT

--- models/News.js
+++ models/News.js
@@ -1,36 +1,17 @@
-// const mongoose = require('mongoose');
-
-// const newsSchema = new mongoose.Schema({
-//   sourceId: String,
-//   sourceName: String,
-//   author: String,
-//   title: String,
-//   description: String,
-//   url: String,
-//   urlToImage: String,
-//   publishedAt: Date,
-//   content: String,
-// }, { timestamps: true }); // timestamps option adds createdAt and updatedAt fields
-
-// const News = mongoose.model('News', newsSchema);
-
-// module.exports = News;
-
 const mongoose = require("mongoose")
-
 const newsSchema = new mongoose.Schema(
   {
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, 
-    search: { type: String },  
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    search: { type: String },
     newsList: [
       {
         sourceId: String,
         sourceName: String,
         author: String,
-        title: { type: String, required: true }, 
+        title: { type: String, required: true },
         description: String,
-        url: { type: String, required: true },  
-        urlToImage: String,  
+        url: { type: String, required: true },
+        urlToImage: String,
         publishedAt: Date,
         content: String,
       },
